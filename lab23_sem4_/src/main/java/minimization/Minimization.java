d931b85f09be19d3863f0a43a92b608714f50bd4
Lesha Oleshkevich <90507705+aleshkey@users.noreply.github.com>
2023-04-13 03:34:22 +0300

    fix: fix logic of first 2 methods

--- lab23_sem4_/src/main/java/minimization/Minimization.java
+++ lab23_sem4_/src/main/java/minimization/Minimization.java
@@ -25,9 +25,16 @@ public class Minimization {
 
         String[] buff = originalString.split(reverseOperator);
 
-        for(var b : buff){
-            res.add(b.substring(1, b.length()-1));
+
+        if (buff.length > 1) {
+            for (var b : buff) {
+                res.add(b.substring(1, b.length() - 1));
+            }
         }
+        else
+            if (buff.length!=0){
+                res.add(originalString);
+            }
 
         return res;
     }
@@ -37,21 +44,21 @@ public class Minimization {
         List<String> a = divideString(s1, isANDSeparator);
         List<String> b = divideString(s2, isANDSeparator);
         StringBuilder res = new StringBuilder();
-        int k=0;
+        int counter = 0;
         for (String s : a) {
             for (int j = 0; j < b.size(); j++) {
                 if (s.equals(b.get(j))) {
+                    counter++;
                     if (res.toString().equals("")) {
                         res.append(s);
                     } else res.append(stringOperator).append(s);
-                    break;
-                } else if (j == b.size() - 1) k++;
+                }
             }
-            if (k > 1) {
-                return "";
+            if (counter == a.size()-1){
+                break;
             }
         }
-        return "("+res+")";
+        return res.equals("") || res.indexOf(String.valueOf(stringOperator)) == -1 ? "" : "("+res+")";
     }
 
 
@@ -62,20 +69,34 @@ public class Minimization {
         List<String> functionalParts = divideStringWithBrackets(originalString, isANDSeparator);
 
         for (int i = 0; i<functionalParts.size(); i++){
-            for (int j =i+1; j<functionalParts.size(); j++){
-                if (!findSharedPart(functionalParts.get(i), functionalParts.get(j), isANDSeparator).equals("")){
-                    if (resultString.toString().equals("")){
+            for (int j = i + 1; j < functionalParts.size(); j++) {
+                if (!findSharedPart(functionalParts.get(i), functionalParts.get(j), isANDSeparator).equals("")) {
+                    if (resultString.toString().equals("")) {
                         resultString.append(findSharedPart(functionalParts.get(i), functionalParts.get(j), isANDSeparator));
-                    }
-                    else resultString.append(reverseOperator).append(findSharedPart(functionalParts.get(i), functionalParts.get(j), isANDSeparator));
+                    } else
+                        resultString.append(reverseOperator).append(findSharedPart(functionalParts.get(i), functionalParts.get(j), isANDSeparator));
                 }
             }
+            functionalParts.remove(i);
+            i--;
         }
         return resultString.toString();
     }
 
-    private static boolean checkExcess(List<String> functionParts, int index, boolean isANDSeparator){
+    private static int checkExcess(List<String> functionParts, int index, boolean isANDSeparator){
         List<List<String>> constituentsMatrix = new ArrayList<>();
+        int res = -1;
+        for (String functionPart : functionParts) {
+            constituentsMatrix.add(divideString(functionPart, isANDSeparator));
+        }
+        List<String> tempPart = constituentsMatrix.get(index);
+        for(int i =0; i < constituentsMatrix.size(); i++){
+            if (unionString(constituentsMatrix.get(i), tempPart).size() == tempPart.size()-1 && checkReversed(constituentsMatrix.get(i), tempPart)){
+                res = i;
+            }
+        }
+        return res;
+        /*List<List<String>> constituentsMatrix = new ArrayList<>();
         for (String functionPart : functionParts) {
             constituentsMatrix.add(divideString(functionPart, isANDSeparator));
         }
@@ -98,10 +119,70 @@ public class Minimization {
             }
         }
         return false;
+    */
+    }
+
+    private static boolean checkReversed(List<String> expression1, List<String> expression2){
+         for (var s : expression2){
+             if (expression1.contains(reverse(s))){
+                 return true;
+             }
+         }
+         return false;
+    }
+
+    private static List<String> unionString(List<String> expression1, List<String> expression2){
+         List<String> res = new ArrayList<>();
+         for (var s : expression2){
+             if (expression1.contains(s)){
+                res.add(s);
+             }
+         }
+         return res;
+    }
+
+    private static String fromListToNF(List<String> list, boolean isANDSeparator){
+         String stringOperator = isANDSeparator ? "&" : "|";
+         String reversedOperator = !isANDSeparator ? "&" : "|";
+         StringBuilder res = new StringBuilder();
+         for (var el : list){
+             if (res.toString().equals(""))
+                res.append(el);
+             else {
+                 res.append(reversedOperator).append(el);
+             }
+         }
+         return res.toString();
     }
 
     private static String NFtoTDF(String originalString, boolean isANDSeparator){
         List<String> functionParts = divideStringWithBrackets(originalString, isANDSeparator);
+        StringBuilder res = new StringBuilder();
+        String stringOperator = isANDSeparator ? "&" : "\\|";
+        String reversedOperator = !isANDSeparator ? "&" : "|";
+        for (int i = 0; i< functionParts.size(); i++){
+            String buff = "";
+            if (checkExcess(functionParts, i, isANDSeparator)!=-1){
+                List<String> tempPartList = Arrays.asList(functionParts.get(i).split(stringOperator));
+                List<String> checkedPartList = Arrays.asList(functionParts.get(checkExcess(functionParts, i, isANDSeparator)).split(stringOperator));
+                String union = fromListToNF(unionString(tempPartList, checkedPartList), isANDSeparator);
+                buff = buff + "("+union+")";
+                functionParts.remove(checkExcess(functionParts, i, isANDSeparator));
+                functionParts.remove(i);
+                i--;
+            }
+            else buff = buff + "("+functionParts.get(i)+")";
+            if (res.toString().equals("")){
+                res.append(buff);
+            }
+            else {
+                if (res.indexOf(buff) == -1) {
+                    res.append(reversedOperator).append(buff);
+                }
+            }
+        }
+        return res.toString();
+        /*List<String> functionParts = divideStringWithBrackets(originalString, isANDSeparator);
         for (int i = 0; i< functionParts.size(); i++){
             if (checkExcess(functionParts, i, isANDSeparator)){
                 var check = originalString.indexOf(functionParts.get(i));
@@ -111,7 +192,7 @@ public class Minimization {
                 i--;
             }
         }
-        return originalString;
+        return originalString;*/
     }
 
     private static boolean exist (String stringWhat, String stringIn, boolean isANDSeparator){
@@ -150,9 +231,16 @@ public class Minimization {
         }
 
         List<Integer> resId = new ArrayList<>();
-        List<List<Integer>> resConstituents =  new ArrayList<>();
 
         Map <Integer, Boolean> switchedConstituents = createHashMap(constituents);
+
+        for (Map.Entry<Integer, Boolean> entry : switchedConstituents.entrySet()) {
+            if (getImplicationsOfConstituents(entry.getKey(), constituentsToImplications).size() == 1){
+                switchedConstituents = turnOnConstituent(switchedConstituents , constituentsToImplications.get(getImplicationsOfConstituents(entry.getKey(), constituentsToImplications).get(0)));
+                resId.add(getImplicationsOfConstituents(entry.getKey(), constituentsToImplications).get(0));
+            }
+        }
+
 
         while (!isStrongTable(switchedConstituents)){
             int bestChoice = chooseBestVariant(switchedConstituents, constituentsToImplications);
@@ -184,7 +272,7 @@ public class Minimization {
         int max = -1;
         int index = -1;
         for (Map.Entry<Integer, Integer> entry : implicationsToNumberUnswitchedConstituents.entrySet()) {
-            if (entry.getValue() > max){
+            if (entry.getValue() >= max){
                 max = entry.getValue();
                 index = entry.getKey();
             }
@@ -214,6 +302,18 @@ public class Minimization {
     }
 
 
+    private static List<Integer> getImplicationsOfConstituents(int constituent, List<List<Integer>> constituentsToImplications){
+         List<Integer> res = new ArrayList<>();
+         for(int i =0;i<constituentsToImplications.size(); i++){
+             if (constituentsToImplications.get(i).contains(constituent)){
+                 res.add(i);
+             }
+         }
+         return res;
+    }
+
+
+
     private static boolean isStrongTable(Map<Integer, Boolean> switchedConstituents){
          for (boolean value : switchedConstituents.values()) {
             if (!value) {
@@ -224,15 +324,15 @@ public class Minimization {
     }
 
     private static void printResult(List<String> implications, List<Integer> resId, boolean isANDSeparator) {
-         String stringOperator = isANDSeparator ? "&" : "|";
+         String reverseOperator = isANDSeparator ? "|" : "&";
          StringBuilder res = new StringBuilder();
         for (Integer integer : resId) {
             if (!res.toString().equals("")) {
-                res.append(stringOperator);
+                res.append(reverseOperator);
             }
             res.append(implications.get(integer));
         }
-        System.out.println("\nИтог минимизации: "+res);
+        System.out.println("\n"+res+"\nИтог минимизации: "+NFtoTDF(res.toString(), isANDSeparator));
     }
 
     public static void minimize(String[] variables, List<List<Boolean>> table, boolean[] result) {
@@ -256,10 +356,10 @@ public class Minimization {
         System.out.println("КНФ: " +CNF);
         System.out.println("ТКНФ: " + TCNF);
 
-        System.out.println("\n\nРасчетно-табличный метод\nСДНФ:\n");
+        System.out.println("\n\nРасчетно-табличный метод\nСДНФ: "+defaultSDNF+"\n");
         printTable(defaultSDNF, true);
 
-        System.out.println("\n\nРасчетно-табличный метод\nСКНФ:\n");
+        System.out.println("\n\nРасчетно-табличный метод\nСКНФ: "+defaultSCNF+"\n");
         printTable(defaultSCNF, false);
     }
 
